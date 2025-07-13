872a3b768f280a0878a5681852d7cd19123e02c5
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-13 13:25:44 -0700

    added textarea and button

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,30 @@
-import ReactDOM from "react-dom/client";
-
-const el = document.getElementById("root");
+import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
+const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
-  return <h1>hi</h1>;
+	const [input, setInput] = useState('');
+	const [code, setCode] = useState('');
+
+	const onClick = () => {
+		console.log(input);
+	};
+	return (
+		<div>
+			<textarea
+				value={input}
+				onChange={(e) => {
+					setInput(e.target.value);
+				}}
+			></textarea>
+			<div>
+				<button onClick={onClick}>Submit</button>
+			</div>
+			<pre>{code}</pre>
+		</div>
+	);
 };
 
 root.render(<App />);
