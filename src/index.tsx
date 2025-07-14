429e45d84009b20769cce426bf80d645169bd420
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-13 19:59:53 -0700

    refactoring plugins

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { useState, useEffect, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el!);
@@ -28,7 +29,7 @@ const App = () => {
 			entryPoints: ['index.js'],
 			bundle: true,
 			write: false,
-			plugins: [unpkgPathPlugin(input)],
+			plugins: [unpkgPathPlugin(), fetchPlugin(input)],
 			define: {
 				'process.env.NODE_ENV': '"production"',
 				global: 'window',
