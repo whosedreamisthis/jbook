131e9f8fbd68ec8559d1e698386fbdd275dc269c
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-13 20:38:39 -0700

    clean up code

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -16,12 +16,14 @@ export const fetchPlugin = (inputCode: string) => {
 					contents: inputCode,
 				};
 			});
-			build.onLoad({ filter: /.css$/ }, async (args: any) => {
+			build.onLoad({ filter: /.*/ }, async (args: any) => {
 				const cachedResult =
 					await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 				if (cachedResult) {
 					return cachedResult;
 				}
+			});
+			build.onLoad({ filter: /.css$/ }, async (args: any) => {
 				const { data, request } = await axios.get(args.path);
 				const escaped = data
 					.replace(/\n/g, '')
@@ -41,11 +43,6 @@ export const fetchPlugin = (inputCode: string) => {
 				return result;
 			});
 			build.onLoad({ filter: /.*/ }, async (args: any) => {
-				const cachedResult =
-					await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-				if (cachedResult) {
-					return cachedResult;
-				}
 				const { data, request } = await axios.get(args.path);
 
 				const result: esbuild.OnLoadResult = {
