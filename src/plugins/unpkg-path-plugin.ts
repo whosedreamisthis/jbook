8a650f8d29e79052d7bf9a10c2c1ae4a423eeb56
Dana Sharon <whosedreamisthis@gmail.com>
2025-07-13 17:43:47 -0700

    caching for performance

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+	name: 'fileCache',
+});
 
 export const unpkgPathPlugin = () => {
 	return {
@@ -35,18 +40,28 @@ export const unpkgPathPlugin = () => {
 						loader: 'jsx',
 						contents: `
               // import message from 'tiny-test-pkg';
-							import react , {useState} from 'react';	
-							console.log(react, useState);
+							import reactSelect from 'react-select';
+
+							console.log(react, useState, reactDOM);
 							
             `,
 					};
 				}
+
+				const cachedResult =
+					await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+				if (cachedResult) {
+					return cachedResult;
+				}
 				const { data, request } = await axios.get(args.path);
-				return {
+				const result: esbuild.OnLoadResult = {
 					loader: 'jsx',
 					contents: data,
 					resolveDir: new URL('./', request.responseURL).pathname,
 				};
+
+				await fileCache.setItem(args.path, result);
+				return result;
 			});
 		},
 	};
